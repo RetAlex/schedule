9a8fde72da937a1c14118ad9ac770120c99c0b5b
RetAlex <aar97@ukr.net>
2019-04-02 20:50:51 +0300

    Added repository annotations

--- main/java/schedule/_main/Runner.java
+++ main/java/schedule/_main/Runner.java
@@ -1,11 +1,17 @@
-package schedule.main;
+package schedule._main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication(scanBasePackages = "schedule.*")
+@EnableWebMvc
+@EnableJpaRepositories(basePackages = "schedule.repositories")
+@EntityScan(basePackages = "schedule.models.entities")
 public class Runner extends SpringBootServletInitializer {
 
     public static void main(String[] args){
